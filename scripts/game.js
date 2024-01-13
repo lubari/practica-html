5bb44575eb8b82b323ae057f26aad45d74fbc043
Ana <48427687+lubari@users.noreply.github.com>
2024-01-12 23:52:29 -0300

    finished course

--- scripts/game.js
+++ scripts/game.js
@@ -3,6 +3,8 @@ const choices = Array.from(document.getElementsByClassName("choice-text"));
 const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById("score");
 const progressBarFull = document.getElementById("progressBarFull");
+const loader = document.getElementById("loader");
+const game = document.getElementById("game");
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
@@ -11,27 +13,50 @@ let availableQuesions = [];
 
 let questions = [];
 
-fetch('questions.json')
-    .then((res) => {
-        return res.json();
-    })
-    .then((loadedQuestions) => {
-        questions = loadedQuestions;
-        startGame();
-    })
-    .catch((err) => {
-        console.error(err);
-    });
-
 //CONSTANTS
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 3;
+const MAX_QUESTIONS = 10;
+
+fetch(
+  'https://opentdb.com/api.php?amount=10&category=11&difficulty=medium&type=multiple'
+)
+  .then((res) => {
+    return res.json();
+  })
+  .then((loadedQuestions) => {
+    questions = loadedQuestions.results.map((loadedQuestion) => {
+      const formattedQuestion = {
+        question: loadedQuestion.question,
+      };
+
+      const answerChoices = [...loadedQuestion.incorrect_answers];
+      formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
+      answerChoices.splice(
+        formattedQuestion.answer - 1, 
+        0, 
+        loadedQuestion.correct_answer
+        );
+
+      answerChoices.forEach((choice, index) => {
+        formattedQuestion["choice" + (index + 1)] = choice;
+      });
+      return formattedQuestion;
+    });
+    startGame();
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+
 
 startGame = () => {
   questionCounter = 0;
   score = 0;
   availableQuesions = [...questions];
   getNewQuestion();
+  game.classList.remove("hidden");
+  loader.classList.add("hidden");
 };
 
 getNewQuestion = () => {
@@ -58,8 +83,8 @@ getNewQuestion = () => {
   acceptingAnswers = true;
 };
 
-choices.forEach(choice => {
-  choice.addEventListener("click", e => {
+choices.forEach((choice) => {
+  choice.addEventListener("click",(e) => {
     if (!acceptingAnswers) return;
 
     acceptingAnswers = false;
@@ -82,7 +107,7 @@ choices.forEach(choice => {
   });
 });
 
-incrementScore = num => {
+incrementScore = (num) => {
   score += num;
   scoreText.innerText = score;
 };
